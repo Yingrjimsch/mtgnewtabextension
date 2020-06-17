04b33e673dfc5b13a217a6f1c16aec1213cbb5a1
Gabriel Nobel <gabriel.nobel@abraxas.ch>
2020-06-17 07:31:15 +0200

    added artists name

--- sketch.js
+++ sketch.js
@@ -8,16 +8,26 @@ function loadImageFromJSON() {
 function setup() {
     mtgCard = loadJSON('https://api.scryfall.com/cards/random', loadImageFromJSON);
     createCanvas(windowWidth, windowHeight);
+    textSize(32);
+    textAlign(CENTER);
 }
 
 function draw() {
     background(0);
     imageMode(CENTER);
-    if(!img) {
-        return;
-    }
+    img ? drawImage() : '';
+    mtgCard ? drawArtistName() : '';
+    
+}
+
+function drawImage() {
     img.width < img.height ? img.resize(windowWidth, 0) : img.resize(0, windowHeight);
-    image(img, windowWidth/2, windowHeight/2);
+    image(img, windowWidth / 2, windowHeight / 2);
+}
+
+function drawArtistName() {
+    fill(255);
+    text(mtgCard.artist, windowWidth/2, windowHeight-16);
 }
 
 function windowResized() {
