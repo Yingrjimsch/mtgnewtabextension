377bc351f706d0e83ec8dc31fbde64dac61357de
Gabriel Nobel <gabriel.nobel@abraxas.ch>
2020-06-17 07:23:23 +0200

    removed preload

--- sketch.js
+++ sketch.js
@@ -1,19 +1,21 @@
 let mtgCard;
 let img;
 
-function preload() {
-    mtgCard = loadJSON('https://api.scryfall.com/cards/random', loadImageFromJSON);
-}
 function loadImageFromJSON() {
     img = loadImage(mtgCard.image_uris.art_crop);
 }
+
 function setup() {
+    mtgCard = loadJSON('https://api.scryfall.com/cards/random', loadImageFromJSON);
     createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
     background(0);
     imageMode(CENTER);
+    if(!img) {
+        return;
+    }
     img.width < img.height ? img.resize(windowWidth, 0) : img.resize(0, windowHeight);
     image(img, windowWidth/2, windowHeight/2);
 }
